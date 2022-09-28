9a2ff590adf2ee6f5fbb7dd5ae24789c9587e22f
HadiChaaban <chaabanh@umich.edu>
2022-09-27 21:13:35 -0400

    Movement.cs adds mouse click movement and keyboard movement in 4 directions for the player in MainScene.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    bool key;
+    Vector2 mousePosition;
+    void Update()
+    {
+        // Move to mouse click position
+        if (Input.GetMouseButtonDown(0)){
+            key = false;
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);   
+        }
+        if (mousePosition != Vector2.zero && key == false){
+            transform.position = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
+        }
+
+        //Movement based on four directions
+        if (Input.GetKey("down")){
+            key = true;
+            transform.Translate(0, (-1 * Time.deltaTime * moveSpeed), 0);
+        }
+        if (Input.GetKey("up")){
+            key = true;
+            transform.Translate(0, (1 * Time.deltaTime * moveSpeed), 0);
+        }
+        if (Input.GetKey("left")){
+            key = true;
+            transform.Translate((-1 * Time.deltaTime * moveSpeed), 0, 0);
+        }
+        if (Input.GetKey("right")){
+            key = true;
+            transform.Translate((1 * Time.deltaTime * moveSpeed), 0, 0);
+        }
+    }
+}
