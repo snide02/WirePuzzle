8d6f722d929aa753057009a58d479da2eb719cd2
JavaJimmy10 <majedahmad03@hotmail.com>
2022-10-03 16:23:49 -0400

    Doors

---
+++ Assets/Scripts/RoomScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    public int rangeToBomb;
+    public int amountOfDoors;
+
+    public bool isOutlet;
+    public bool isBomb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
