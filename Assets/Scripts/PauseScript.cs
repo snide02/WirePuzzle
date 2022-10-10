d1e3945be4eed4383c76e2250daf9000818eaefe
haris192037 <hariskhan1337@gmail.com>
2022-10-10 14:49:53 -0400

    PauseMenu

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    bool pause = false;
+
+    void Start()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+         if (Input.GetKeyDown ("escape")) {
+             if(pause == true){
+                 Time.timeScale = 1.0f;
+                 PauseMenu.SetActive(false);
+                 pause = false;
+             } else {
+                 Time.timeScale = 0.0f;
+                 PauseMenu.SetActive(true);
+                 pause = true;
+             }
+         }
+    }
+
+    public void RestartButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1.0f;
+    }
+}
